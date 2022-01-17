9e8d5adff6e2bb00922f301a2126473e4a091fd1
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-17 17:29:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
-
+import Values from "values.js";
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [list, setList] = useState(new Values("#f15025").all(5));
+  const [isError, setIsError] = useState(false);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(5);
+
+      setList(colors);
+    } catch (error) {
+      setIsError(true);
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator project</h3>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            value={color}
+            placeholder="#f15025"
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+            className={`${isError ? "error" : null}`}
+          />
+          <button type="submit" className="btn">
+            Generate
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
